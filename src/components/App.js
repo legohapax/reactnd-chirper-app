60769ffd556f775095ae50ce1d3f93ab2d9fd998
VojtaDiatka <vojta.diatka@gmail.com>
2018-05-27 12:14:14 +0200

    tweet component rendering tweets done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux"
 import { handleInitialData } from "../actions/shared"
+import Dashboard from "./Dashboard"
+import LoadingBar from "react-redux-loading"
 
 class App extends Component {
   componentDidMount() {
@@ -9,10 +11,23 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
-      </div>
+        <LoadingBar />
+          {this.props.loading === true
+          ? null
+          : <Dashboard />
+          
+        }
+        </div>
     )
   }
 }
 
-export default connect()(App)
+function mapStateToProps({ authedUser }) {
+    return {
+      loading: authedUser === null
+    }  
+  
+
+}
+
+export default connect(mapStateToProps)(App)
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react"
+import { connect } from "react-redux"
+import Tweet from "./Tweet"
 
 class Dashboard extends Component {
     render() {
         return (
-          <div>
-          Dashboard
-          </div>
+            <div>
+                <h3 className="center">Your timeline</h3>
+                    <ul className="dashboard-list">
+                        {this.props.tweetIds.map((id) =>(
+                            <li key={id}>
+                                <div>
+                                    <Tweet id={id} />
+                                </div>
+                            </li>
+                        ))}
+                    </ul>    
+            </div>
         )
     }
 }
@@ -13,8 +24,8 @@ class Dashboard extends Component {
 function mapStateToProps({tweets}) {
     return {
         tweetIds: Object.keys(tweets)
-          .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
+            .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
     }  
 }
 
-export default Dashboard
+export default connect(mapStateToProps)(Dashboard)  
